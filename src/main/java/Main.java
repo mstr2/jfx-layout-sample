fd255da2255585a0e147253afb76e71dad24336d
mstr2 <43553916+mstr2@users.noreply.github.com>
2021-11-16 16:06:08 +0100

    Changed VM option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty("javafx.sg.layoutStats", "true");
+        System.setProperty("javafx.sg.printLayout", "true");
         Application.launch(App.class);
     }
 
